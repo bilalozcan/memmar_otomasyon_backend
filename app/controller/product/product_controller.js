7b619cbf44ab48693d131a8fe73034656475ec8d
Bilal Özcan <bilal.ozcan@btkurumsal.com.tr>
2021-05-29 18:39:21 +0300

    update product and filter get product completed

--- app/controller/product/product_controller.js
+++ app/controller/product/product_controller.js
@@ -4,19 +4,32 @@ const Product = require('./product_model');
 
 const productCreatePath = '/product/create';
 const productAllPath = '/product/all';
+const productListPath = '/product/list';
+const productUpdatePath = '/product/update';
 
 router.get(productAllPath, (request, response) => {
-   
-    request.query
     Product.getAllProducts().then(result => {
         response.json({ 'data': result[0], 'success': true });
     });
 });
-router.post(productCreatePath,(request, response) => {
+
+router.get(productListPath, (request, response) => {
+    Product.getQueryProducts(request.query).then(result => {
+        response.json({ 'data': result[0], 'success': true });
+    });
+});
+
+router.post(productCreatePath, (request, response) => {
     let product = { ...request.body };
     Product.createProduct(product).then(result => {
         response.status(200).json({ 'data': result[0][0], 'success': true });
     });
+})
+router.put(productUpdatePath, (request, response) => {
+    let product = { ...request.body };
+    Product.updateProduct(product).then(result => {
+        response.status(200).json({ 'data': result[0][0], 'success': true });
+    })
 })
 
 module.exports = {
--- app/controller/product/product_model.js
+++ app/controller/product/product_model.js
@@ -2,7 +2,7 @@ const config = require('../../db_config');
 const sql = require('mssql');
 
 
-async function getAllProducts(query) {
+async function getAllProducts() {
 
     try {
         let pool = await sql.connect(config);
@@ -14,11 +14,28 @@ async function getAllProducts(query) {
     }
 }
 
+async function getQueryProducts(query) {
+    console.log(query);
+    console.log(query.companyId);
+    console.log(query.searchValue);
+    //'%' + query.searchValue + '%'
+    try {
+        let pool = await sql.connect(config);
+        let products = await pool.request()
+            .input('companyId', sql.Int, Number(query.companyId))
+            .input('searchValue', sql.NVarChar, '%' + query.searchValue + '%')
+            .query("SELECT * FROM product WHERE companyId = @companyId AND (name LIKE @searchValue OR barcode LIKE @searchValue)");
+        return products.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
 async function createProduct(product) {
     try {
         let pool = await sql.connect(config);
         let insertProduct = await pool.request()
-            .input('companyId', sql.Int, product.companyId)
             .input('name', sql.NVarChar, product.name)
             .input('purchasePrice', sql.Real, product.purchasePrice)
             .input('stock', sql.Int, product.stock)
@@ -27,12 +44,13 @@ async function createProduct(product) {
             .input('countryCode', sql.NVarChar, product.countryCode)
             .input('salePrice', sql.Real, product.salePrice)
             .input('productType', sql.Int, product.productType)
-            .input('isActive', sql.Int, product.isActive)
-            .input('createdUser', sql.NVarChar, product.createdUser)
+            .input('isActive', sql.Bit, product.isActive)
+            .input('createdUser', sql.Int, product.createdUser)
             .input('createdDate', sql.DateTime, product.createdDate)
             .input('supplierld', sql.Int, product.supplierld)
             .input('sold', sql.Int, product.sold)
             .input('updateDate', sql.DateTime, product.updateDate)
+            .input('companyId', sql.Int, product.companyId)
             .execute('addProduct');
         return insertProduct.recordsets;
     }
@@ -42,7 +60,40 @@ async function createProduct(product) {
     }
 }
 
+async function updateProduct(product) {
+    try {
+        let pool = await sql.connect(config);
+        let insertProduct = await pool.request()
+            .input('id', sql.Int, product.id)
+            .input('name', sql.NVarChar, product.name)
+            .input('purchasePrice', sql.Real, product.purchasePrice)
+            .input('stock', sql.Int, product.stock)
+            .input('remaining', sql.Int, product.remaining)
+            .input('barcode', sql.NVarChar, product.barcode)
+            .input('countryCode', sql.NVarChar, product.countryCode)
+            .input('salePrice', sql.Real, product.salePrice)
+            .input('productType', sql.Int, product.productType)
+            .input('isActive', sql.Bit, product.isActive)
+            .input('createdUser', sql.Int, product.createdUser)
+            .input('createdDate', sql.DateTime, product.createdDate)
+            .input('supplierld', sql.Int, product.supplierld)
+            .input('sold', sql.Int, product.sold)
+            .input('updateDate', sql.DateTime, product.updateDate)
+            .input('companyId', sql.Int, product.companyId)
+            .execute('updateProduct');
+        return insertProduct.recordsets;
+    }
+    catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+
+
 module.exports = {
     getAllProducts: getAllProducts,
     createProduct: createProduct,
+    updateProduct: updateProduct,
+    getQueryProducts: getQueryProducts
 }
