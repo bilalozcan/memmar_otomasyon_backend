2a0586ad71d839f6ac5dbeccda06b7836ad89203
Bilal Özcan <bilal.ozcan@btkurumsal.com.tr>
2021-06-01 23:50:55 +0300

    statistic second and third added

--- app/controller/statistics/statistic_controller.js
+++ app/controller/statistics/statistic_controller.js
@@ -3,10 +3,26 @@ var router = express.Router();
 const Statistic = require('./statistic_model');
 
 const statisticOnePath = '/statistic/one';
+const statisticSecondPath = '/statistic/second';
+const statisticThirdPath = '/statistic/third';
 
 router.get(statisticOnePath, (request, response) => {
     console.log(request.query);
     Statistic.statistic1(request.query).then(result => {
+        response.json({ 'data': result[0], 'success': true });
+    });
+});
+
+router.get(statisticSecondPath, (request, response) => {
+    console.log(request.query);
+    Statistic.statistic2(request.query).then(result => {
+        response.json({ 'data': result[0], 'success': true });
+    });
+});
+
+router.get(statisticThirdPath, (request, response) => {
+    console.log(request.query);
+    Statistic.statistic3(request.query).then(result => {
         response.json({ 'data': result[0], 'success': true });
     });
 });
--- app/controller/statistics/statistic_model.js
+++ app/controller/statistics/statistic_model.js
@@ -1,6 +1,7 @@
 const config = require('../../db_config');
 const sql = require('mssql');
 
+//Bir şirkete ait kullanıcı için toplam satılan ürün miktarı ve toplam 
 async function statistic1(query) {
     console.log(query.companyId);
     queryString = "SELECT [dbo].[user].id as userId, [dbo].[user].fullName,  " +
@@ -25,8 +26,51 @@ async function statistic1(query) {
     }
 }
 
+//Bir şirkete ait ürün kategorilerinden satış miktarları
+async function statistic2(query) {
+    console.log(query.companyId);
+    try {
+        let pool = await sql.connect(config);
+        let table = await pool.request()
+            .input('companyId', sql.Int, query.companyId)
+            .query(
+                "SELECT SUM(product.sold) as totalSold, productType.name " +
+                "from product INNER JOIN productType ON productType.id = product.productType  " +
+                "WHERE product.companyId = @companyId " +
+                "GROUP BY productType.name ORDER BY totalSold  DESC "
+            );
+        return table.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+//Ürünlerin tedarik edilme miktarları
+async function statistic3(query) {
+    console.log(query.companyId);
+    try {
+        let pool = await sql.connect(config);
+        let table = await pool.request()
+            .input('companyId', sql.Int, query.companyId)
+            .query(
+                "select product.name, supply.quantity " +
+                "from product  " +
+                "INNER JOIN supply on supply.productId = product.id " +
+                "WHERE product.companyId = @companyId "+
+                "ORDER BY supply.quantity DESC "
+            );
+        return table.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
     statistic1: statistic1,
+    statistic2: statistic2,
+    statistic3: statistic3
 }
 
 // "SELECT [dbo].[user].id as userId, [dbo].[user].fullName,  "+
